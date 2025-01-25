a63b2ae42276b3dd7cd2c0e7dbdc799b5bca4837
Airidas10 <airidas.jaraminas@gmail.com>
2025-01-25 11:21:03 +0200

    Remove unnecessary Log statements

--- app/Http/Controllers/SearchController.php
+++ app/Http/Controllers/SearchController.php
@@ -11,14 +11,11 @@ class SearchController extends Controller
 {
     public function search(Request $request, $type, $searchString=null)
     {
-
         $words = Word::where('word', 'like', '%' . $searchString . '%')->orWhere('translation', 'like', '%' . $searchString . '%')->get();
 
         if($request->header('X-Requested-With') === 'XMLHttpRequest'){
-            \Log::debug("IF");
             return ['status' => 'success', 'msg' => 'Data fetched successfully', 'data' => $words];
         } else{
-            \Log::debug("ELSE");
             return Inertia::render('Index', [
                 'wordsList' => $words
             ]);
