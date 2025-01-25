5e59ef44eca8143f7718296161679d2e3112daff
Airidas10 <airidas.jaraminas@gmail.com>
2025-01-25 23:37:22 +0200

    Another wrong text fix

--- resources/js/Pages/TagCreator.vue
+++ resources/js/Pages/TagCreator.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="max-w-2xl mx-auto p-6 bg-white rounded-lg shadow-lg mt-8">
-        <h1 class="text-3xl font-semibold text-gray-800 mb-8 text-center">Create New Tag</h1>
+        <h1 class="text-3xl font-semibold text-gray-800 mb-8 text-center">{{ mode == 'create' ? 'Create New Tag' : 'Edit Tag' }}</h1>
         <form>
             <div class="mb-6">
                 <label for="tag" class="block text-base font-medium text-gray-700 mb-2">Tag</label>
