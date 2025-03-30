eb981ee1ad8d6a9d26c7c903b29d02415cb1e128
Airidas10 <airidas.jaraminas@gmail.com>
2025-03-30 13:33:49 +0300

    Remove delete button from create tag view

--- resources/js/Pages/TagCreator.vue
+++ resources/js/Pages/TagCreator.vue
@@ -22,6 +22,7 @@
                     Save
                 </button>
                 <button
+                    v-if="mode != 'create'"
                     type="submit"
                     class="flex-1 bg-red-600 text-white py-2 px-4 rounded-lg shadow hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-red-500 focus:ring-offset-2"
                     @click.prevent.stop="deleteButtonClicked"
