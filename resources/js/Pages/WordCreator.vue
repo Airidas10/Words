198cd1c0a1d5ec7741b1587b5c74e0e6ec2da625
Airidas10 <airidas.jaraminas@gmail.com>
2025-03-30 13:32:56 +0300

    Remove delete button from create word view

--- resources/js/Pages/WordCreator.vue
+++ resources/js/Pages/WordCreator.vue
@@ -84,7 +84,7 @@
                 <button type="submit" class="flex-1 bg-blue-600 text-white py-2 px-4 rounded-lg shadow hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2" @click.prevent.stop="saveButtonClicked">
                     Save
                 </button>
-                <button type="submit" class="flex-1 bg-red-600 text-white py-2 px-4 rounded-lg shadow hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-red-500 focus:ring-offset-2" @click.prevent.stop="deleteButtonClicked">
+                <button v-if="mode != 'create'" type="submit" class="flex-1 bg-red-600 text-white py-2 px-4 rounded-lg shadow hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-red-500 focus:ring-offset-2" @click.prevent.stop="deleteButtonClicked">
                     Delete
                 </button>
             </div>
